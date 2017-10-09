9c795c55387a609f580275a756152365ccdd0c28
seth-skocelas <s.skocelas@gmail.com>
2017-10-09 18:06:02 -0500

    Initial Commit

---
+++ devslopes-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by Seth Skocelas on 10/9/17.
+//  Copyright © 2017 Seth Skocelas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
