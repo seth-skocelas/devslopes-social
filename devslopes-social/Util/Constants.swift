8fec41ee74f838ee6169449fc882f4568f2abc19
seth-skocelas <s.skocelas@gmail.com>
2017-10-09 23:24:50 -0500

    Sign In UI Custom Classes added

---
+++ devslopes-social/Util/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Seth Skocelas on 10/9/17.
+//  Copyright © 2017 Seth Skocelas. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
---
+++ devslopes-social/View/FancyField.swift
@@ -0,0 +1,35 @@
+//
+//  FancyField.swift
+//  devslopes-social
+//
+//  Created by Seth Skocelas on 10/9/17.
+//  Copyright © 2017 Seth Skocelas. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        
+        return bounds.insetBy(dx: 10, dy: 5)
+        
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        
+        return bounds.insetBy(dx: 10, dy: 5)
+        
+    }
+
+}
