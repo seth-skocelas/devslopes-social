201293c4c435f02efac008b06fe4383db9eda3d3
seth-skocelas <s.skocelas@gmail.com>
2017-10-21 18:08:11 -0500

    Added Initial Feed UI

--- devslopes-social/View/CircleView.swift
+++ devslopes-social/View/CircleView.swift
@@ -1,24 +1,27 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  devslopes-social
 //
-//  Created by Seth Skocelas on 10/9/17.
+//  Created by Seth Skocelas on 10/21/17.
 //  Copyright © 2017 Seth Skocelas. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
-        
-        super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+
+    override func draw(_ rect: CGRect) {
+        
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
         
     }
 
--- devslopes-social/View/FancyField.swift
+++ devslopes-social/View/FancyField.swift
@@ -17,6 +17,7 @@ class FancyField: UITextField {
         layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
         layer.borderWidth = 1.0
         layer.cornerRadius = 2.0
+        self.sizeToFit()
         
     }
     
