83e49a189d5e65a7034b3b4279b257bc97f8c02c
seth-skocelas <s.skocelas@gmail.com>
2017-11-10 12:00:31 -0600

    Added Initial Feed UI

---
+++ devslopes-social/View/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  devslopes-social
+//
+//  Created by Seth Skocelas on 11/4/17.
+//  Copyright © 2017 Seth Skocelas. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+}
