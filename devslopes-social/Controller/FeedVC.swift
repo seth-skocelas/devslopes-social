cf2338312cd43e256977debd2f9ec346c35cca65
seth-skocelas <s.skocelas@gmail.com>
2017-11-14 15:30:04 -0600

    Post data displayed in console

--- devslopes-social/Controller/FeedVC.swift
+++ devslopes-social/Controller/FeedVC.swift
@@ -14,6 +14,8 @@ import Firebase
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +25,19 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             
-            print(snapshot.value)
-            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                        
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
         
         // Do any additional setup after loading the view.
@@ -38,11 +51,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 3 //temp
+        return posts.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("READ THIS: \(post.caption)")
         
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
